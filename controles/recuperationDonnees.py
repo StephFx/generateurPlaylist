94ae2ccf884b0d78c8eed7967723c5f302d37b87
François-Xavier LEBRET <f.xavier.lebret@gmail.com>
2014-11-18 16:33:23 +0100

    optimisation recup donnees

--- controles/recuperationDonnees.py
+++ controles/recuperationDonnees.py
@@ -1,4 +1,3 @@
-
 from Initialisations.connexion import mes_morceaux
 from Initialisations.connexion import connexion
 from Initialisations.argument import argumentsParser
@@ -11,6 +10,9 @@ Created on 15 oct. 2014
 @author: fx
 '''
 
+'''Liste des arguments du programme
+Attributs=['g','ar','sg','alb','t','r']'''
+
 '''Liste des arguments du programme'''
 Attributs=[('g', "genre"),('ar', "artiste"),('sg', "sousgenre"),('alb', "album"),('t', "titre")]
 
@@ -20,24 +22,22 @@ def rechercheBase(Attributs, valeurRechercher, arg):
     trouve = False
     '''Recherche dans la liste des arguments'''
     while (i<len(Attributs) and trouve == False):
-        if Attributs[i][0] == arg:
-            playList=list(connexion.execute(sqlalchemy.select([mes_morceaux]).where(mes_morceaux.c.Attributs[i][1] == valeurRechercher)))
-            trouve = True
-            print(playList)
-        i+=1
-    return playList
-        
 
+        if Attributs[i][0] == arg:
+            nameColumn=Attributs[i][1]
+            playList=list(connexion.execute(sqlalchemy.select([mes_morceaux]).where(getattr(mes_morceaux.c, nameColumn) == valeurRechercher)))
+            trouve = True
+
+    i+=1
+    return playList
 
 def verificationChoisi(selection, arg):
-    
+
     select = rechercheBase(Attributs, selection, arg)
-    
     if select != []:
         return True
     else :
         return False
-
 
 '''Module qui permet de recuperer une liste de morceaux selon les arguments de la commande'''
 def recuperationDonnees(argumentsParser):
@@ -50,11 +50,13 @@ def recuperationDonnees(argumentsParser):
             '''On trie l'ensemble aleatoire'''
             random.shuffle(listeAFiltrer)
             '''On range le dernier morceaux de la playlist dans la liste final et on la supprime de l'ensemble'''
-            morceauChoisi = listeAFiltrer.pop()
+            morceauChoisi = listeAFiltrer[0]
+
+            '''On remelange la liste a filtrer'''
+            random.shuffle(listeAFiltrer)
 
             '''On l'ajout a la playlist final'''
             listeFinale.append(morceauChoisi)
-
 
             '''On decrément la quantite du morceaux ajoute au total de la quantiter voulue'''
             quantiteEscomptee -= morceauChoisi.duree
@@ -65,19 +67,24 @@ def recuperationDonnees(argumentsParser):
 
     '''On creer une liste de liste'''
     collectionListesFiltrees = list()
-    
-    '''On va parcourir l'ensemble des Attributs possible'''
-    for recherche in Attributs:
+
+    '''Initialisation d'un compteur'''
+    i=0
+
+    while (i<len(Attributs)):
+
         '''On regarde s'il est rentree dans la commande'''
-        if getattr(argumentsParser, recherche):
+        if getattr(argumentsParser, Attributs[i][0]):
             '''On parcourt l'ensemble d'un attribut'''
-            for unArgument in getattr(argumentsParser, recherche):
-                '''Si l'argument est le genre'''
-                playList = rechercheBase(Attributs, unArgument[0], recherche)
+            for unArgument in getattr(argumentsParser, Attributs[i][0]):
+                playList = rechercheBase(Attributs, unArgument[0], Attributs[i][0])
+                print(playList)
                 '''On applique la fonction de selection morceaux'''
                 final=filtrerListe(collectionListesFiltrees, playList, unArgument[1] * argumentsParser.duree_playlist / 100 * 60)
 
                 if (final is not None):
                     '''on passe en parametre la quantite à garder à l'interieur de la sous playlist'''
                     collectionListesFiltrees.append(final)
+        i+=1
+
     return collectionListesFiltrees
